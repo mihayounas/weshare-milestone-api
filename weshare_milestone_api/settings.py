1a30024985ac75619ecd94fe5f6690d84be32ff9
mihayounas <mihaelayounas55@gmail.com>
2022-12-10 02:14:12 +0000

    FIXING DEPLOY ERRORS

--- weshare_milestone_api/settings.py
+++ weshare_milestone_api/settings.py
@@ -62,7 +62,10 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost', 'https://weshare-api-app.herokuapp.com']
+ALLOWED_HOSTS = [
+    os.environ.get('ALLOWED_HOST'),
+    'localhost',
+]
 
 
 # Application definition
