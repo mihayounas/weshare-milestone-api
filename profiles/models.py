77012275d16fad5fdc0e69379db109ee2f2c5013
mihayounas <mihaelayounas55@gmail.com>
2022-12-21 12:02:43 +0000

    fixing default profile picture

--- profiles/models.py
+++ profiles/models.py
@@ -10,7 +10,7 @@ class Profile(models.Model):
     name = models.CharField(max_length=255, blank=True)
     content = models.TextField(blank=True)
     image = models.ImageField(
-        upload_to='images/', default='https://res.cloudinary.com/df4j1glpo/image/upload/v1668434573/samples/default_profile_m2uned.jpg'
+        upload_to='images/', default='https://res.cloudinary.com/df4j1glpo/image/upload/v1671279356/default_profile_ogr78q.jpg'
     )
     blocked_users = models.ManyToManyField(User, related_name="blocked")
 
