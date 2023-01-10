118538385a75caf9f415cfea43d5fbcd98cfaa6a
mihayounas <mihaelayounas55@gmail.com>
2023-01-10 11:18:08 +0000

    trying to understand why my frontend does not send data to blocked and returning an empty array

--- blocked/serializers.py
+++ blocked/serializers.py
@@ -1,10 +1,8 @@
-from django.shortcuts import get_object_or_404
 from rest_framework import serializers
 from blocked.models import Block
 
 
-# Serializers
 class BlockSerializer(serializers.ModelSerializer):
     class Meta:
         model = Block
-        fields = ['user', 'reason', 'duration']
+        fields = ('user', 'reason', 'duration')
