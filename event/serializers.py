31ff9100d38bf7b4de9bd068470200f2cac5b8c7
mihayounas <mihaelayounas55@gmail.com>
2023-01-25 20:54:56 +0000

    change events because is not creating in database

--- event/serializers.py
+++ event/serializers.py
@@ -4,8 +4,6 @@ from django.contrib.auth.models import User
 
 
 class EventSerializer(serializers.ModelSerializer):
-    owner = serializers.ReadOnlyField(source='owner.username')
-
     class Meta:
         model = Event
         fields = ('owner', 'id', 'title', 'start_time', 'end_time', 'location', 'description', 'created_at', 'updated_at')
