e98011c8399ecc4c634cc5ce5e95a3ec572880d0
mihayounas <mihaelayounas55@gmail.com>
2023-01-27 17:22:04 +0000

    setting up share functionality

--- shares/models.py
+++ shares/models.py
@@ -4,12 +4,14 @@ from posts.models import Post
 
 
 class Shares(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(
+        Post, related_name='shares', on_delete=models.CASCADE
+    )
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         ordering = ['-created_at']
 
     def __str__(self):
-        return f'{self.id} {self.owner.username} shares {self.post.title}'
+        return f'{self.owner} {self.post}'
