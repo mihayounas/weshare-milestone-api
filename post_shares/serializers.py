20d3890d8ae4ad6ba0fc5461ddb01b69b1fea7dc
mihayounas <mihaelayounas55@gmail.com>
2023-01-28 18:59:21 +0000

    work on creating a share

--- post_shares/serializers.py
+++ post_shares/serializers.py
@@ -7,9 +7,3 @@ class PostShareSerializer(serializers.ModelSerializer):
         model = PostShare
         fields = ('id', 'owner', 'post', 'created_at')
         read_only_fields = ('id', 'created_at')
-
-    def create(self, validated_data):
-        request = self.context.get("request")
-        if request and hasattr(request, "user"):
-            validated_data["owner"] = request.user
-        return super().create(validated_data)
