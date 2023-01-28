3add4b9bbdddf00703442a561df9b219ad735e66
mihayounas <mihaelayounas55@gmail.com>
2023-01-28 19:14:43 +0000

    shares

--- post_shares/views.py
+++ post_shares/views.py
@@ -2,7 +2,6 @@ from rest_framework import generics, permissions
 from weshare_milestone_api.permissions import IsOwnerOrReadOnly
 from post_shares.models import PostShare
 from post_shares.serializers import PostShareSerializer
-from rest_framework import Response
 
 
 class PostSharesList(generics.ListCreateAPIView):
@@ -12,13 +11,6 @@ class PostSharesList(generics.ListCreateAPIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = PostShare.objects.all()
     serializer_class = PostShareSerializer
-
-    def post(self, request):
-        serializer = PostShareSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save(owner=request.user)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class PostSharesDetail(generics.RetrieveDestroyAPIView):
