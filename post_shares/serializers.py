5e1920b27bdeb7497689e49b8b7bb204ad41da22
mihayounas <mihaelayounas55@gmail.com>
2023-01-28 15:30:02 +0000

    getting fields validated in order to create share instance

--- post_shares/serializers.py
+++ post_shares/serializers.py
@@ -1,17 +1,8 @@
 from rest_framework import serializers
-from post_shares.models import PostShare
-
+from .models import PostShare
 
 class PostShareSerializer(serializers.ModelSerializer):
-    owner = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
-    post = serializers.PrimaryKeyRelatedField(queryset=Post.objects.all())
-
     class Meta:
         model = PostShare
-        fields = ('owner', 'post')
-
-    def create(self, validated_data):
-        owner = validated_data.get('owner')
-        post = validated_data.get('post')
-        post_share = PostShare.objects.create(owner=owner, post=post)
-        return post_share
+        fields = ('id', 'owner', 'post', 'created_at')
+        read_only_fields = ('id', 'created_at')
