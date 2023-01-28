adf60d93e621bf5ff1e865b79f58611fab420353
mihayounas <mihaelayounas55@gmail.com>
2023-01-28 15:41:49 +0000

    change post_share model

--- post_shares/models.py
+++ post_shares/models.py
@@ -2,10 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 from posts.models import Post
 
+
 class PostShare(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    post = models.ForeignKey(
+        Post, related_name='to_share', on_delete=models.CASCADE
+    )
     created_at = models.DateTimeField(auto_now_add=True)
-    
+
     class Meta:
-        unique_together = ('owner', 'post')
+        ordering = ['-created_at']
+        unique_together = ['owner', 'post']
--- post_shares/serializers.py
+++ post_shares/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import PostShare
 
+
 class PostShareSerializer(serializers.ModelSerializer):
     class Meta:
         model = PostShare
