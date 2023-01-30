3334c1d8e6e569770eab30deb88501c2ef5e4c4d
mihayounas <mihaelayounas55@gmail.com>
2023-01-30 06:43:01 +0000

    final deply

--- weshare_milestone_api/settings.py
+++ weshare_milestone_api/settings.py
@@ -90,7 +90,6 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django_extensions',
     'django.contrib.sessions',
     'django.contrib.messages',
     'cloudinary_storage',
