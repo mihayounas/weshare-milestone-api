4c3385ae37fa68325060b59b1ae9d3fb65999089
mihayounas <mihaelayounas55@gmail.com>
2023-03-14 23:47:05 +0000

    trying for manual testing

--- weshare_milestone_api/settings.py
+++ weshare_milestone_api/settings.py
@@ -62,17 +62,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-if DEBUG:
-    MIDDLEWARE += (
-        'debug_toolbar.middleware.DebugToolbarMiddleware',
-    )
-    INSTALLED_APPS += (
-        'debug_toolbar',
-    )
-    INTERNAL_IPS = ('127.0.0.1', )
-    DEBUG_TOOLBAR_CONFIG = {
-        'INTERCEPT_REDIRECTS': False,
-    }
+
 
 ALLOWED_HOSTS = [
    os.environ.get('ALLOWED_HOST'),
@@ -138,6 +128,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 ]
 
 
