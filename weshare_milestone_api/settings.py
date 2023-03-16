911f7ee22f150c6f82c92799be7c0ad919ed4a58
mihayounas <mihaelayounas55@gmail.com>
2023-03-16 00:20:06 +0000

    change debug to false

--- weshare_milestone_api/settings.py
+++ weshare_milestone_api/settings.py
@@ -61,7 +61,7 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 ALLOWED_HOSTS = [
@@ -155,18 +155,18 @@ WSGI_APPLICATION = 'weshare_milestone_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# if 'DEV' in os.environ:
-#    DATABASES = {
-#        'default': {
-#            'ENGINE': 'django.db.backends.sqlite3',
-#           'NAME': BASE_DIR / 'db.sqlite3',
-#        }
-#    }
-# else:
-#   DATABASES = {
-#        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-#    }
-#   print('connected')
+if 'DEV' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    }
+print('connected')
 
 
 # Password validation
